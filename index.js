6f787c132bfd7acf22c248908876035e33d2a985
Degreet <degreetpro@gmail.com>
2021-12-04 15:38:50 +0200

    fix bugs and more

--- index.js
+++ index.js
@@ -1,69 +1,95 @@
 module.exports = function sender(ctx, next) {
 	ctx.msg = {
 		send(text, extra) {
-			return ctx
-				.replyWithHTML(text, extra)
-				.catch(() => console.warn('msg.send error'))
+			try {
+				return ctx.replyWithHTML(text, extra)
+			} catch (e) {
+				console.error(e)
+			}
 		},
 
-		sendto(userId, text, extra) {
-			return ctx.telegram
-				.sendMessage(userId, text, { parse_mode: 'html', ...extra })
-				.catch(() => console.warn('msg.sendto error'))
+		sendTo(userId, text, extra) {
+			try {
+				return ctx.telegram.sendMessage(userId, text, { parse_mode: 'html', ...extra })
+			} catch (e) {
+				console.error(e)
+			}
 		},
 
 		edit(text, extra) {
-			return ctx
-				.editMessageText(text, { parse_mode: 'html', ...extra })
-				.catch(() => console.warn('msg.edit error'))
+			try {
+				return ctx.editMessageText(text, { parse_mode: 'html', ...extra })
+			} catch (e) {
+				console.error(e)
+			}
 		},
 
 		del() {
-			return ctx.deleteMessage().catch(() => console.warn('msg.del error'))
+			try {
+				return ctx.deleteMessage()
+			} catch (e) {
+				console.error(e)
+			}
 		},
 
-		broadcast(users, callback, action) {
-			if (!callback || typeof callback !== 'function') callback = null
-			if (!action || typeof action !== 'function') callback = null
-			const resultUsers = [[]]
-			let activeUsersIndex = 0
+		broadcast(params) {
+			try {
+				let { users, callback, action, extra, message } = params
 
-			users.forEach((userId) => {
-				const last = resultUsers[resultUsers.length - 1]
-				if (last.length < 30) last.push(userId)
-				else resultUsers.push([userId])
-			})
+				if (!callback || typeof callback !== 'function') callback = null
+				if (!action || typeof action !== 'function') action = null
+				const resultUsers = [[]]
+				let activeUsersIndex = 0
 
-			function end() {
-				if (callback) callback()
+				users.forEach((userId) => {
+					const last = resultUsers[resultUsers.length - 1]
+					if (last.length < 30) last.push(userId)
+					else resultUsers.push([userId])
+				})
+
+				function end() {
+					if (callback) callback()
+					return true
+				}
+
+				async function step() {
+					const startedAt = Date.now()
+					const usrs = resultUsers[activeUsersIndex++]
+					if (!usrs || usrs.length <= 0) return end()
+
+					await Promise.all(
+						usrs.map(async (userId) => {
+							await ctx.telegram
+								.sendCopy(userId, message || ctx.message, extra)
+								.catch(console.error)
+							if (action) action(userId)
+						})
+					)
+
+					return setTimeout(step, Math.max(0, startedAt + 1000 - Date.now()))
+				}
+
+				step()
 				return true
+			} catch (e) {
+				console.error(e)
 			}
-
-			async function step() {
-				const startedAt = Date.now()
-				const usrs = resultUsers[activeUsersIndex++]
-				if (!usrs || usrs.length <= 0) return end()
-
-				await Promise.all(
-					usrs.map(async (userId) => {
-						await ctx.telegram.sendCopy(userId, ctx.message).catch(() => {})
-						if (action) action(userId)
-					})
-				)
-
-				return setTimeout(step, Math.max(0, startedAt + 1000 - Date.now()))
-			}
-
-			step()
-			return true
 		},
 
 		toast(text) {
-			return ctx.answerCbQuery(text)
+			try {
+				return ctx.answerCbQuery(text)
+			} catch (e) {
+				console.error(e)
+			}
 		},
 
 		alert(text) {
-			return ctx.answerCbQuery(text, { alert: true })
+			try {
+				return ctx.answerCbQuery(text, { alert: true })
+			} catch (e) {
+				console.error(e)
+			}
 		},
 	}
 
